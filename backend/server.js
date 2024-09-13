1313c48c6096e221906b3cae37372abfe5d912aa
Sidharth Sankar Patro <sidharth.patro.2010@gmail.com>
2024-09-13 10:35:15 +0000

    delted route folder

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-
+//app.use;
 app.get('/', (req, res) => {
     console.log('[GET ROUTE]');
     res.send('HELLO FROM HOMEPAGE');
